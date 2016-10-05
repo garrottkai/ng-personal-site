2d7221650436b380c8ecdb62a12fc2dec6cea2ad
Kai Garrott <garrottkai@gmail.com>
2016-10-05 16:03:22 -0600

    add dependencies

---
+++ public_html/angular/app.js
@@ -0,0 +1,1 @@
+// main ng app file
--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,22 @@
 <?php require_once("php/templates/head.php");?>
 
+<body class="sfooter">
+	<div class="sfooter-content">
+		<div>
+			<main>
+				<div>
+
+					<!-- do yer thang, angular -->
+					<div ng-view></div>
+
+				</div>
+			</main>
+		</div>
+
+	</div><!-- sfooter content -->
+
+	<!-- footer here, or maybe not-->
+
+
+</body>
+</html>
