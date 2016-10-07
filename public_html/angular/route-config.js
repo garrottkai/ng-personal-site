190c68911352e28cd82b9ffc8bc695b2f961e4c4
Kai Garrott <garrottkai@gmail.com>
2016-10-07 17:32:50 -0600

    configure routes

--- public_html/angular/route-config.js
+++ public_html/angular/route-config.js
@@ -1,1 +1,38 @@
 // ng routing
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+
+	// route for the home page
+		.when('/', {
+			controller  : 'homeController',
+			templateUrl : 'angular/templates/home-template.php'
+		})
+
+		// route for the portfolio page
+		.when('/portfolio', {
+			controller  : 'portfolioController',
+			templateUrl : 'angular/templates/portfolio-template.php'
+		})
+
+		// route for the about page
+		.when('/about', {
+			controller  : 'aboutController',
+			templateUrl : 'angular/templates/about-template.php'
+		})
+
+		// route for the contact page
+		.when('/contact', {
+			controller  : 'contactController',
+			templateUrl : 'angular/templates/contact-template.php'
+		})
+
+		// otherwise redirect to home
+		.otherwise({
+			redirectTo: '/'
+		});
+
+	//use the HTML5 History API
+
+	$locationProvider.html5Mode(true);
+
+});
