1bbc7036f05b1b19d0929672ec3adb547f696da3
Kai Garrott <garrottkai@gmail.com>
2016-10-07 17:58:19 -0600

    populate remaining templates incl. jQuery validated form; todo: ng-ify that

--- public_html/angular/templates/about-template.php
+++ public_html/angular/templates/about-template.php
@@ -1,1 +1,10 @@
 // template for About view
+<?php require_once("header-template.php"); ?>
+<div class="row mcontent">
+	<div class="col-xs-12 sec">
+		<h2>Fresh ideas to energize your business</h2>
+		<br>
+		<p>I am a full-stack web developer based in Albuquerque, New Mexico.</p>
+		<p>My passion is in bringing clients' ideas to life.</p>
+	</div>
+</div> <!-- /row -->
