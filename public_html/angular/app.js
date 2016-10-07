e667669d30e38f9d2174400d9ebf11fc74ac256f
Kai Garrott <garrottkai@gmail.com>
2016-10-07 17:41:25 -0600

    add header

--- public_html/angular/app.js
+++ public_html/angular/app.js
@@ -1,1 +1,2 @@
 // main ng app file
+var app = angular.module("kaiApp", ["ngRoute", "ngMessages", "ngAnimate", "ngTouch"]);
---
+++ public_html/angular/templates/header-template.php
@@ -0,0 +1,20 @@
+<header ng-controller="headerController">
+	<bootstrap-breakpoint></bootstrap-breakpoint>
+	<div class="row topnav-row">
+		<div class="navbar">
+			<div class="navbar-inner">
+
+				<a href="home">
+					<div class="col-xs-12 col-md-4 topnav-l">Kai Garrott</div>
+				</a>
+				<ul class="nav">
+					<li class="col-xs-4 col-md-2 topnav"><a href="contact">Contact</a></li>
+					<li class="col-xs-4 col-md-2 topnav"><a href="about">About</a></li>
+					<li class="col-xs-4 col-md-2 topnav"><a href="portfolio">Portfolio</a></li>
+				</ul>
+			</div>
+
+		</div>
+	</div> <!-- /row -->
+	<hr class="topnavline">
+</header>
