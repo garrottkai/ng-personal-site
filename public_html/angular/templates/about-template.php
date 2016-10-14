c83bb78da141e008cbb685580c155907e66d3e84
Kai Garrott <garrottkai@gmail.com>
2016-10-13 18:07:50 -0600

    cosmetic stuff; no more background image; footer opaque

--- public_html/angular/templates/about-template.php
+++ public_html/angular/templates/about-template.php
@@ -4,7 +4,8 @@
 	<div class="col-xs-12 sec">
 		<h2>Fresh ideas to energize your business</h2>
 		<br>
-		<p>I am a full-stack web developer based in Albuquerque, New Mexico.</p>
-		<p>My passion is in bringing clients' ideas to life.</p>
+		<p>I am a full-stack web developer based in Albuquerque, New Mexico. My passion is in bringing clients' ideas to life.</p>
+		<p>I come from a background of diverse interests and skills and have, at different times, variously familiarized myself with design, technical and promotional writing, systems administration, world history, botany, stained glass, engineering, aviation, and the Oxford comma.</p>
+		<p>Upon occasion, I've been known to jump off a cliff or two</p>
 	</div>
 </div> <!-- /row -->
--- public_html/php/templates/footer.php
+++ public_html/php/templates/footer.php
@@ -1,11 +1,11 @@
 <!--static footer to be injected-->
 <footer class="navbar navbar-fixed-bottom">
 	<hr class="navline">
-	<div class="col-md-4 col-md-offset-4">
+	<div class="col-md-4 col-md-offset-4 bottomnav">
 		<ul class="nav navbar-nav">
 			<li class="nav-item footer-l navbar-text">&copy 2016 Kai Garrott</li>
 		</ul>
-		<div class="footer-i navbar-text"><a href="https://github.com/garrottkai" target="_blank"><img
-					src="media/gh-light-64px.png"></a></div>
+<!--		<div class="footer-i navbar-text"><a href="https://github.com/garrottkai" target="_blank"><img-->
+<!--					src="media/gh-light-64px.png"></a></div>-->
 	</div>
 </footer>
