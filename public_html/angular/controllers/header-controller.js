cf0ca0cc62a01b09a9c5163b5858b77bf426ea9b
Kai Garrott <garrottkai@gmail.com>
2016-10-17 15:46:25 -0600

    very slight debugging success

--- public_html/angular/controllers/header-controller.js
+++ public_html/angular/controllers/header-controller.js
@@ -1,10 +1,16 @@
 // controller for header
-app.controller("headerController", ["$scope", function($scope) {
+app.controller("headerController", ["$scope", "$location", function($scope, $location) {
 
 	// collapse navbar on xs screen
 	$scope.$watch("breakpoint", function() {
 		$scope.navCollapsed = ($scope.breakpoint === "xs");
 	});
+
+	$scope.where = function($location) {
+		$scope.loc = $location.url();
+		console.log($scope.loc)
+	};
+	$scope.where($location);
 
 
 	$scope.breakpoint = null;
