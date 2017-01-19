1f33806c55b5dbe20d7c5412a98d542e8d51aea7
Kai Garrott <garrottkai@gmail.com>
2017-01-19 12:11:24 -0700

    cleanup; fixing css

--- public_html/angular/templates/header-template.php
+++ public_html/angular/templates/header-template.php
@@ -7,7 +7,6 @@
 
 				<a href="home">
 					<img class="col-md-4" id="topnav-logo" src="media/kg-icon-placeholder.png"/><!--attr for angular soon ng-class="$scope.where == '/' ? 'logoLeft' : 'logoCenter'"-->
-<!--					<div class="col-xs-12 col-md-4 topnav-l">Kai Garrott</div>-->
 				</a>
 				<ul class="nav">
 					<!--<li class="col-xs-4 col-md-2 topnav"><a href="contact">Contact</a></li>-->
