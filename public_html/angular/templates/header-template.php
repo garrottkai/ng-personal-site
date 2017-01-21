f741d40e34ca432feb220294e8298046d60febc6
Kai Garrott <garrottkai@gmail.com>
2017-01-20 18:19:23 -0700

    fixing navbar behavior

--- public_html/angular/templates/header-template.php
+++ public_html/angular/templates/header-template.php
@@ -1,21 +1,28 @@
 <header ng-controller="headerController">
 	<bootstrap-breakpoint></bootstrap-breakpoint>
 	<div class="row">
-		<div class="navbar" id="nav-main">
+		<div class="navbar navbar-header" id="nav-main">
 			<div class="navbar-inner">
-
-				<a href="home">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-bts"> <!-- ng-click="!navCollapsed" -->
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+				<a href="home" class="navbar-brand">
 					<div  class="col-xs-4">
 					<img id="topnav-logo" src="media/kg-icon-placeholder.png"/>
 				</div>
 				</a>
+				<div class="collapse navbar-collapse" id="nav-bts">
 				<ul class="nav">
 					<!--<li class="col-xs-4 col-md-2 topnav"><a href="contact">Contact</a></li>-->
 					<li class="col-xs-4 col-md-2 topnav"><a href="about">About</a></li>
 					<li class="col-xs-4 col-md-2 topnav"><a href="portfolio">Portfolio</a></li>
 				</ul>
 			</div>
+			</div>
+
 		</div>
+						<hr class="topnavline">
 	</div> <!-- /row -->
-	<hr class="topnavline">
 </header>
