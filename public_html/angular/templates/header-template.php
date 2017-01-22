ca6be6fec2b28c2691eeed3481085c4cca611c17
Kai Garrott <garrottkai@gmail.com>
2017-01-21 21:17:31 -0700

    fixing navbar

--- public_html/angular/templates/header-template.php
+++ public_html/angular/templates/header-template.php
@@ -1,29 +1,31 @@
 <header ng-controller="headerController">
 	<bootstrap-breakpoint></bootstrap-breakpoint>
-	<div class="navbar">
-		<div class="navbar navbar-header" id="nav-main">
-			<div class="navbar-inner">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-bts"> <!-- ng-click="!navCollapsed" -->
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
+	<nav class="navbar navbar-default">
+		<div class="navbar-header" id="nav-main">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-bts" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 				<a href="home" class="navbar-brand">
 					<div  class="col-xs-4">
 					<img id="topnav-logo" src="media/kg-icon-placeholder.png"/>
 				</div>
 				</a>
-						</div>
+			</div>
 				<div class="collapse navbar-collapse" id="nav-bts">
-				<ul class="nav">
+				<ul class="nav navbar-nav navbar-right">
 					<!--<li class="col-xs-4 col-md-2 topnav"><a href="contact">Contact</a></li>-->
-					<li class="col-xs-4 col-md-2 topnav"><a href="about">About</a></li>
-					<li class="col-xs-4 col-md-2 topnav"><a href="portfolio">Portfolio</a></li>
+					<li><a href="about">About</a></li>
+					<li><a href="portfolio">Portfolio</a></li>
 				</ul>
 			</div>
-			</div>
+
 
 
 						<hr class="topnavline">
-	</div> <!-- /row -->
+	</nav> <!-- /row -->
+<div class="spacer">
+</div>
 </header>
