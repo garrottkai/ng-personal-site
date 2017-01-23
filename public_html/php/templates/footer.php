df590e00cecaff8462677c9e5a245688522c0e18
Kai Garrott <garrottkai@gmail.com>
2017-01-22 22:15:35 -0700

    fixing footer

--- public_html/php/templates/footer.php
+++ public_html/php/templates/footer.php
@@ -1,12 +1,22 @@
 <footer class="navbar navbar-fixed-bottom">
 	<hr class="navline">
-	<div class="col-md-4 col-md-offset-4 bottomnav">
+	<div class="row bottomnav">
+	<div class="col-xs-6 col-xs-offset-3">
 		<ul class="nav navbar-nav">
-			<li class="nav-item footer-l navbar-text">&copy 2017 Kai Garrott</li>
+			<li class="nav-item footer-l">&copy 2017 Kai Garrott</li>
 		</ul>
-<div class="footer-i navbar-text"><a href="https://github.com/garrottkai" target="_blank"><img
-src="media/GitHub-Mark-64px.png"></a></div>
-<div class="footer-i navbar-text"><a href="https://linkedin.com/in/kai-garrott" target="_blank"><img
-src="media/In-Black-66px-R.png"></a></div>
 	</div>
+	<div class="col-xs-3 nav navbar-nav">
+		<span class="footer-i">
+			<a href="https://github.com/garrottkai" target="_blank">
+				<img src="media/GitHub-Mark-64px.png">
+			</a>
+		</span>
+		<span class="footer-i">
+			<a href="https://linkedin.com/in/kai-garrott" target="_blank">
+				<img src="media/In-Black-66px-R.png">
+			</a>
+		</span>
+</div>
+</div>
 </footer>
