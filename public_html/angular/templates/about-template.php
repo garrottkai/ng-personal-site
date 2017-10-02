e37edd601862f9881177828e5da5723645e1ecdd
Kai Garrott <garrottkai@gmail.com>
2017-10-01 20:23:15 -0600

    updating 'about' page...need to replace most of the site

--- public_html/angular/templates/about-template.php
+++ public_html/angular/templates/about-template.php
@@ -2,12 +2,12 @@
 
 <div class="row mcontent">
 	<div class="about-img-box">
-			<img src="media/kai-pg.jpg" class="about-img">
+			<img src="media/portrait.jpg" class="about-img">
 	</div>
 	<div class="sec about-sec">
-		<h2>Fresh ideas to energize your business</h2>
+		<h2>Howdy there! I'm Kai.</h2>
 		<br>
-		<p>I am a full-stack web developer, designer, and technical generalist based in Albuquerque, New Mexico. I take great pride in bringing clients' ideas to life.</p>
+		<p>I'm a full-stack developer, designer, and technical generalist based in Albuquerque, New Mexico. I serve as CDO of <a href="https://lyradevelopment.com" target="_blank">Lyra Development</a>, directing a development team in creating custom web and mobile software solutions for a broad spectrum of clients.</p>
 		<p>I come from a background of diverse interests and skills and have, at different times, mastered or experimented with graphic design, technical and promotional writing, systems administration, world history, botany, stained glass, engineering, aviation, and the Oxford comma.</p>
 		<p>When I'm not busy creating or learning new things, you can find me planting trees that I've grown from seed, hiking, flying paragliders, and exploring the wilderness of the American West.</p>
 	</div>
